b0f31e06f8b5fba94eb48b72fc850a6ba755c088
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-11 20:09:56 -0800

    marker setup

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,65 +9,27 @@ class Map extends React.Component {
     const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/light-v10', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: [-77.0353, 38.8895], // starting position [lng, lat]
     zoom: 2 // starting zoom
     });
-  
-    map.on('load', () => {
-      // Add an image to use as a custom marker
-      
-      map.loadImage(
-          'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-          (error, image) => {
-              if (error) throw error;
-              map.addImage('custom-marker', image);
-              // Add a GeoJSON source with 2 points
-              map.addSource('points', {
-                  'type': 'geojson',
-                  'data': {
-                      'type': 'FeatureCollection',
-                      'features': this.props.points.map(point => 
-                        (
-                          {
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [
-                                    point.coordinates.lat,
-                                    point.coordinates.long
-                                ]
-                            },
-                            'properties': {
-                                'title': point.title
-                            }
-                        }
-                        )
-                        )
-                         
-                      
-                  }
-              });
-
-              // Add a symbol layer
-              map.addLayer({
-                  'id': 'points',
-                  'type': 'symbol',
-                  'source': 'points',
-                  'layout': {
-                      'icon-image': 'custom-marker',
-                      // get the title name from the source's "title" property
-                      'text-field': ['get', 'title'],
-                      'text-font': [
-                          'Open Sans Semibold',
-                          'Arial Unicode MS Bold'
-                      ],
-                      'text-offset': [0, 1.25],
-                      'text-anchor': 'top'
-                  }
-              });
-          }
+    var monument = [-77.0353, 38.8895];
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      'Construction on the Washington Monument began in 1848.'
       );
-    });
+       
+      // create DOM element for the marker
+      var el = document.createElement('div');
+      el.id = 'marker';
+      el.style.width = "50px"
+      el.style.height = "50px"
+      el.style.backgroundColor = "blue"
+       
+      // create the marker
+      new mapboxgl.Marker(el)
+      .setLngLat(monument)
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
+
   }
   
   render(){
