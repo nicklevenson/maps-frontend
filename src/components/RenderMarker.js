54adb6a8b29dcf64fd5fcf2b202832f98bd6f7f9
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-13 10:14:50 -0800

    marker

--- src/components/RenderMarker.js
+++ src/components/RenderMarker.js
@@ -2,17 +2,16 @@ import mapboxgl from 'mapbox-gl'
 
 const RenderMarker = (props) => {
   var coords = [props.marker.coordinates.lng, props.marker.coordinates.lat];
-  // var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-  //   `<h2>${marker.title}</h2>
-  //   <p>${marker.info}</p>`
-  // )
 // create DOM element for the marker
     var el = document.createElement('div');
     el.className = 'marker';
     new mapboxgl.Marker(el)
     .setLngLat(coords)
-    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-    .setHTML('<h3>' + props.marker.title + '</h3><p>' + props.marker.info + '</p>'))
+      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+      .setHTML(
+        `<h3>${props.marker.title}</h3>
+        <textarea>${props.marker.info}</textarea>`
+      ))
     .addTo(props.map);
 
 
