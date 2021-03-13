e6442d58438fb3d19a280b19451db243e6a5d64f
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-13 10:06:59 -0800

    pop up

--- src/components/RenderMarker.js
+++ src/components/RenderMarker.js
@@ -11,23 +11,26 @@ const RenderMarker = (props) => {
     el.className = 'marker';
     new mapboxgl.Marker(el)
     .setLngLat(coords)
-    // .setPopup(popup) // sets a popup on this marker
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h3>' + props.marker.title + '</h3><p>' + props.marker.info + '</p>'))
     .addTo(props.map);
 
-    let markerEvent = (marker, handleMarkerSelect) => {
-      el.addEventListener("click", function renderInfo(event) {
-        handleMarkerSelect(marker)
-        const container = document.getElementById("marker-info-container")
-        var x = event.clientX;
-        var y = event.clientY;
-        container.style.position = 'absolute';
-        container.style.left = x + 'px';
-        container.style.top = y + 'px';
-        container.style.width = "30vw";
+
+
+    // let markerEvent = (marker, handleMarkerSelect) => {
+    //   el.addEventListener("click", function renderInfo(event) {
+    //     handleMarkerSelect(marker)
+    //     const container = document.getElementById("marker-info-container")
+    //     var x = event.clientX;
+    //     var y = event.clientY;
+    //     container.style.position = 'absolute';
+    //     container.style.left = x + 'px';
+    //     container.style.top = y + 'px';
+    //     container.style.width = "30vw";
    
-      })
-    }
-    markerEvent(props.marker, props.handleMarkerSelect) 
+    //   })
+    // }
+    // markerEvent(props.marker, props.handleMarkerSelect) 
 }
 
 
