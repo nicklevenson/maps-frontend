5516c56d61f585108bef9d37f5c9e95219de4fac
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-13 08:18:20 -0800

    textarea

--- src/components/MarkerInfo.js
+++ src/components/MarkerInfo.js
@@ -7,7 +7,7 @@ export default class MarkerInfo extends React.Component {
       <div className="marker-info">
         {this.props.marker.title ? <div onClick={e=>this.props.handleMarkerSelect({})}>X</div> : null}
         <h1>{this.props.marker.title}</h1>
-        <p>{this.props.marker.info}</p>
+        <textarea>{this.props.marker.info}</textarea>
       </div>
     )
   }
