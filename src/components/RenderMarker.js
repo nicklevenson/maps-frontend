2c5d24243b546957182570156ac6d7d049493c63
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-15 20:19:12 -0700

    logged out render:

--- src/components/RenderMarker.js
+++ src/components/RenderMarker.js
@@ -2,10 +2,11 @@ import mapboxgl from 'mapbox-gl'
 
 
 const RenderMarker = (props) => {
-  if (props.marker.user) {
-    var coords = [props.marker.lng, props.marker.lat];
+  const coords = [props.marker.lng, props.marker.lat];
+  if (sessionStorage.jwt) {
+    
 // create DOM element for the marker
-    var el = document.createElement('div');
+    const el = document.createElement('div');
     el.className = 'marker';
     el.style.backgroundImage = `url(${props.marker.user.image})`
     if (props.currentUser.id === props.marker.user.id) {
@@ -48,6 +49,19 @@ const RenderMarker = (props) => {
     // console.log(props.marker.user)
    
     
+  }else {
+    const el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage = `url(${props.marker.user.image})`
+      const marker = new mapboxgl.Marker(el)
+      .setLngLat(coords)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(
+          `<h3>${props.marker.title}</h3>
+          <textarea>${props.marker.info}</textarea>
+          `
+        ))
+        .addTo(props.map);
   }
   
 }
