606e178e1de100608aa7b16abc3f37534930f30e
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-18 16:59:13 -0700

    fb login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import googleLogin from '../assets/btn_google.png'
+import facebookLogin from '../assets/facebook-login.png'
 import {connect} from 'react-redux'  
 import {fetchUser} from '../actions/UserActions.js'
 import { Redirect } from 'react-router-dom'
@@ -21,11 +22,14 @@ class Login extends React.Component {
   render(){
     return(
       <div className="login-page">
-        <h2>Login/Signup with Google</h2>
+        <h2>Please Login/Signup with one of the folllowing providers</h2>
         {this.props.heading ? <h5><i>{this.props.heading}</i></h5>: null}
-        <a href="http://localhost:3000/authenticate"><img src={googleLogin} alt="Login with Google"/></a>
+        <a href={`${process.env.REACT_APP_BACKEND_URL}/authenticate`}><img src={googleLogin} alt="Login with Google"/></a>
+        <a href={`${process.env.REACT_APP_BACKEND_URL}/authenticate-facebook`}><img src={facebookLogin} alt="Login with Facebook"></img></a>
         {sessionStorage.jwt ? <Redirect to="my-maps" /> : null}
 
+        
+       
       </div>
     )
   }
