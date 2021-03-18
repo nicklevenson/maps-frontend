6702993fb37867e4c01c2992bea9ce52fa731a04
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-18 09:56:23 -0700

    filter

--- src/components/MapFilter.js
+++ src/components/MapFilter.js
@@ -40,7 +40,7 @@ class MapFilter extends React.Component {
 
         <form onChange={e=>this.handleChange(e)}>
             <input type="text" list="usersFilter" placeholder="Search for a map"></input>
-            <input type="reset" id="reset" value="X"></input>
+            <input type="reset" id="reset" value="X" onClick={e => this.handleChange(e)}></input>
             <datalist id="usersFilter">
               {this.renderMapOptions().map(m=><option key={m.id}>{m.title}</option>)}
             </datalist>
